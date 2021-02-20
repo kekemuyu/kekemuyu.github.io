faf823529743e706fcf91ee3908ca6eafa424a27
kekemuyu <kekemuyu@126.com>
2021-02-20 18:31:45 +0800

    fix ble

--- webBle/js/index.js
+++ webBle/js/index.js
@@ -6,7 +6,7 @@ $("#scanBtn")[0].addEventListener('click', function() {
 
 			filters: [{
 			    services: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']
-			  }],
+			  }]
 
 			// acceptAllAdvertisements: true,
 			// keepRepeatedDevices: true,
