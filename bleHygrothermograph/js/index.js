7930976b545628a98b5dd14f8177a6ffe3824085
kekemuyu <kekemuyu@126.com>
2021-05-06 20:49:47 +0800

    fix bus

--- bleHygrothermograph/js/index.js
+++ bleHygrothermograph/js/index.js
@@ -124,7 +124,7 @@ function overrideServices(services) {
 							$('.energy .number').text(objValue.humi);
 						}
 
-				$('#pingBtn')[0].onclick;
+				$('#pingBtn')[0].click();
 					});
 				}
 				if (properties.indexOf("WRITE") != -1) {
